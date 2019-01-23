379610c68434b8f8c7a1d0458c81dc64f2e5cdd9
artemwork <said.kirill@gmail.com>
2019-01-23 16:54:07 +0300

    work seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
+     * http://simple-training.com/basics-of-laravel5/laravel5-seeds/
+     *
      * Seed the application's database.
      *
      * @return void
@@ -12,5 +14,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+    }
+}
+
+class PostsSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('Posts')->delete();
+        Post::create([
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'excerpt' => 'First Post body',
+            'content' => 'Content First Post body',
+            'published' => true,
+            //'published_at' => DB::raw('NOW()'), // для DateTime
+            'published_at' => DB::raw('CURRENT_TIMESTAMP'), // для timestamp
+        ]);
+
+        Post::create([
+            'title' => 'Second Post',
+            'slug' => 'second-post',
+            'excerpt' => 'Second Post body',
+            'content' => 'Content Second Post body',
+            'published' => false,
+            'published_at' => DB::raw('CURRENT_TIMESTAMP'),
+        ]);
+
+        Post::create([
+            'title' => 'Third Post',
+            'slug' => 'third-post',
+            'excerpt' => 'Third Post body',
+            'content' => 'Content Third Post body',
+            'published' => false,
+            'published_at' => DB::raw('CURRENT_TIMESTAMP'),
+        ]);
     }
 }
