b41cf80b1d49c0118cef8c24cda56fc4c034e633
artemama <artsiom.ivahno@gmail.com>
2019-01-24 20:31:50 +0300

    Declaration of App\Http\Controllers\itemController::index($categoryId) should be compatible with App\Http\Controllers\Controller::index()

--- app/Http/Controllers/itemController.php
+++ app/Http/Controllers/itemController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class itemController extends Controller {
 
-	public function index() {
+	public function index($categoryId) {
 
-		$items = [1, 2];
+		$items = [];
+		$items = Item::where('category_id', $categoryId)->get();
 
 
 		return view('items',[
